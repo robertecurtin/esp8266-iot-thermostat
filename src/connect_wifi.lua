b356cb83cd8ef3abcec0fa783f023badb793c9b7
Robert Curtin <robertecurtin@gmail.com>
2020-02-14 22:07:51 -0500

    Tidying and fixes

--- src/connect_wifi.lua
+++ src/connect_wifi.lua
@@ -2,10 +2,10 @@ local wifi_configuration = require 'src/wifi_configuration'
 
 return function()
   wifi.setmode(wifi.STATION)
-  print(wifi.sta.config({
-    ssid = wifi_configuration.name,
+  wifi.sta.config({
+    ssid = wifi_configuration.ssid,
     pwd = wifi_configuration.password,
     connected_cb = function(t) print('Connected to', t.SSID) end,
     got_ip_cb = function(t) print('IP: ', t.IP) end
-  }))
+  })
 end
