dba608ebc0a36aa6e5c4a4fe051d5fdf8e2abaf2
Robert Curtin <robertecurtin@gmail.com>
2020-02-14 21:21:34 -0500

    Fixed wifi config location to match documentation

--- src/connect_wifi.lua
+++ src/connect_wifi.lua
@@ -1,10 +1,12 @@
-local wifi_configuration = require 'wifi_configuration'
+local wifi_configuration = require 'src/wifi_configuration'
 
 return function()
   wifi.setmode(wifi.STATION)
   print(wifi.sta.config({
     ssid = wifi_configuration.name,
     pwd = wifi_configuration.password,
-    connected_cb = function(t) print('Connected with ' .. t.SSID) end
+    connected_cb = function(t)
+      print('Connected to', t.SSID)
+    end
   }))
 end
